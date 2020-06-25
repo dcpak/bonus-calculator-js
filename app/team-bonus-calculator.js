60dd70f4ce87380ed72370742e2c2ae1709e0a5b
Przemysław Kasprzak <przemyslaw.kasprzak@orange.com>
2020-06-25 12:33:26 +0200

    Implementation.

--- app/team-bonus-calculator.js
+++ app/team-bonus-calculator.js
@@ -11,5 +11,8 @@
  * @return {number}
  */
 module.exports = function teamBonusCalculator(params) {
-  return -1;
+  let r = 0;
+  if(params.sales > params.quota && params.members > 0)
+    r = (params.sales - params.quota) * params.commission / 100 / params.members;
+  return r;
 };
