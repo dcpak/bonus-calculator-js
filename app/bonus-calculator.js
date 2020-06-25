7cd24fefeedf73e24bb7818130e53f0bd5781886
Przemysław Kasprzak <przemyslaw.kasprzak@orange.com>
2020-06-25 12:26:56 +0200

    Tests and implementation.

--- app/bonus-calculator.js
+++ app/bonus-calculator.js
@@ -1,2 +1,20 @@
-module.exports = function bonusCalculator() {
+/**
+ * @typedef params
+ * @property {number} sales
+ * @property {number} quota
+ * @property {number} commission
+ * @property {number} tax
+ */
+/**
+ *
+ * @param {params} params
+ * @return {number}
+ */
+module.exports = function bonusCalculator(params) {
+  let r = 0;
+  if(params.quota < params.sales)
+    r = (params.sales - params.quota) * params.commission / 100;
+  if(r)
+    r -= r * params.tax / 100;
+  return r;
 };
--- app/bonus-calculator.spec.js
+++ app/bonus-calculator.spec.js
@@ -2,8 +2,33 @@ const bonusCalculator = require('./bonus-calculator');
 
 describe('Bonus calculation', function () {
 
-    it('works', function () {
-        expect(true).toBeTruthy();
+    [
+        {
+            sales: 12000,
+            quota: 11000,
+            commission: 10,
+            tax: 10,
+            result: 90
+        },
+        {
+            sales: 13000,
+            quota: 15000,
+            commission: 10,
+            tax: 10,
+            result: 0
+        },
+        {
+            sales: 15000,
+            quota: 15000,
+            commission: 10,
+            tax: 10,
+            result: 0
+        },
+    ].forEach(function(params) {
+        it('bonus for sales ' + params.sales + ' with quota ' + params.quota + ', commission ' + params.commission + ' tax ' + params.tax + ' equals ' + params.result, function() {
+            expect(bonusCalculator(params)).toEqual(params.result);
+        });
+
     });
 
 });
