fd94e674a5e47db0ba058d868b2f7407fc1ea91e
Przemysław Kasprzak <przemyslaw.kasprzak@orange.com>
2020-06-25 12:28:22 +0200

    Interface for team bonus calculator.

---
+++ app/team-bonus-calculator.js
@@ -0,0 +1,15 @@
+/**
+ * @typedef params
+ * @property {number} sales
+ * @property {number} quota
+ * @property {number} commission
+ * @property {number} members
+ */
+/**
+ *
+ * @param {params} params
+ * @return {number}
+ */
+module.exports = function teamBonusCalculator(params) {
+  return -1;
+};
